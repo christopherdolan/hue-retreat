4d5f335459d5ad5675447424c7eb89b414c10bbd
Chris Dolan <cdolan@steelhouse.com>
2016-11-02 14:54:45 -0700

    Diz's changes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var Hue = require('philips-hue');
 var hue = new Hue();
+var diz = 0;
 
 var configFile = process.env.HOME + '/.philips-hue.json';
 
-hue.login(configFile)
-  .then(function(conf){
-    return hue.light(1).off();
-  })
-  .then(function(res){
-    console.log(res);
-  })
-  .catch(function(err){
+setInterval(function(){
+  diz = (diz + 1000)%65535;
+  hue.login(configFile).then(function(conf) {
+    var state = {
+      on: true,
+      bri: 240,
+      hue: diz,
+      sat: 200
+    };
+      console.log(state);
+      hue.light(1).setState(state).then(console.log);
+  }).catch(function(err){
     console.error(err.stack || err);
   });
+},200);
